77bcfc140dded0560a49d5988f3469154012d9f4
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-19 10:38:02 +0300

    ajout des vrais textes au section contenant l'avis des proches

--- src/components/avis.jsx
+++ src/components/avis.jsx
@@ -14,7 +14,7 @@ export function Avis(){
                         </div>
                     </div>
                     <div className="message">
-                        <p>Le service qu'offre LUMINI School est très utile pour les habitants de Madagascar surtout ceux de Toamasina car la plupart des étudiants, jeunes chercheurs en informatique sont en manque de cadre pédagogique.</p>
+                        <p><em>"</em> Le service qu'offre LUMINI School est très utile pour les habitants de Madagascar surtout ceux de Toamasina car la plupart des étudiants, jeunes chercheurs en informatique sont en manque de cadre pédagogique. <em>"</em></p>
                     </div>
                 </div>
                 <div className="card">
@@ -22,9 +22,14 @@ export function Avis(){
                         <div className="img-container">
                             <img src="images/profile.png" alt="profile"/>
                         </div>
-                        <div>
-                            <h5>IALY Francisco</h5>
+                        <div className="user-name">
+                            <h4>Marie Michëlla</h4>
+                            <h5>Apprentie en info.</h5>
                         </div>
+                    </div>
+                    <div className="message">
+                        <p><em>"</em> J'adore la méthode que vous employer pour former vos apprentis. Les formateurs sont cool, on s'entend bien avec eux, le courant se passe bien et on se sens bien formé. <br />
+                        Vive LUMINI School ✊, <em>#</em>luminischool <em>"</em></p>
                     </div>
                 </div>
                 <div className="card">
@@ -38,7 +43,7 @@ export function Avis(){
                         </div>
                     </div>
                     <div className="message">
-                        <p>Il est temps que tous les jeunes, adultes, enfants de nos povinces soient beaucoup familiarisés au monde de l'informatique. Ainsi ils apprennent les grands moyens afin de réaliser des grandes idées.</p>
+                        <p><em>"</em> Il est temps que tous les jeunes, adultes, enfants de nos povinces soient beaucoup familiarisés au monde de l'informatique. Ainsi ils apprennent les grands moyens afin de réaliser des grandes idées. <em>"</em></p>
                     </div>
                 </div>
                 <div className="card">
@@ -46,9 +51,13 @@ export function Avis(){
                         <div className="img-container">
                             <img src="images/profile.png" alt="profile"/>
                         </div>
-                        <div>
-                            <h5>IALY Francisco</h5>
+                        <div className="user-name">
+                            <h4>Tandra Mario J.</h4>
+                            <h5>DRH d'une société</h5>
                         </div>
+                    </div>
+                    <div className="message">
+                        <p><em>"</em> Avec un tel projet, matûre et ambitieux, on peut espérer qu'on aura plus des manques des ressources humaines d'ici un ans. Chaque post sera occupé de professionnel déterminé, compétant et prêt à donner à fond leurs connaissances. <em>"</em></p>
                     </div>
                 </div>
                 <div className="card">
@@ -62,7 +71,7 @@ export function Avis(){
                         </div>
                     </div>
                     <div className="message">
-                        <p>Je n'ai jamais su que se former en informatique peut être si facile que je le croiais. Mais grâce à LUMINI School tout s'est dévenu possible et je comprends mieux que l'informatique est une base fiable. "Big thx 👌" à leur équipe.</p>
+                        <p><em>"</em> Je n'ai jamais su que se former en informatique peut être si facile que je le croiais. Mais grâce à LUMINI School tout s'est dévenu possible et je comprends mieux que l'informatique est une base fiable. "Big thx 👌" à leur équipe. <em>"</em></p>
                     </div>
                 </div>
                 <div className="card">
@@ -70,9 +79,13 @@ export function Avis(){
                         <div className="img-container">
                             <img src="images/profile.png" alt="profile"/>
                         </div>
-                        <div>
-                            <h5>IALY Francisco</h5>
+                        <div className="user-name">
+                            <h4>R. Juliot</h4>
+                            <h5>Formateur</h5>
                         </div>
+                    </div>
+                    <div className="message">
+                        <p><em>"</em> Enfin un cadre de formation matûre et fiable pour les apprenants. LUMINI School ravive l'espoir qu'avait nos jeunes en les préparant des formations à objectif bien basé et qui projete vers le monde du futur. On compte sur vous. <em>"</em></p>
                     </div>
                 </div>
             </section>
