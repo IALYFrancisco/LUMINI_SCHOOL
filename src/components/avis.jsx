3eb33549c4ef299975c9ed472b08c1e653c6e083
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-19 05:09:21 +0300

    création de section pour l'avis des utilisateurs

---
+++ src/components/avis.jsx
@@ -0,0 +1,7 @@
+export function Avis(){
+    return(
+        <section>
+
+        </section>
+    )
+}
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../components/header"
+import { Avis } from "../components/avis"
 import '../../public/styles/home.css'
 
 function Home(){
     return (
         <>
             <Header></Header>
+            <Avis></Avis>
         </>
     )
 }
