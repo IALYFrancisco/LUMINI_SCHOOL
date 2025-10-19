44088308876b0f1c6437e500605af7d5baa21b36
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-19 09:12:32 +0300

    développement de la section avis des utilisateurs

--- src/components/avis.jsx
+++ src/components/avis.jsx
@@ -1,7 +1,81 @@
 export function Avis(){
     return(
-        <section>
-
+        <section className="avis">
+            <h2>Ce qu'ils disent à propos de nous 😊 :</h2>
+            <section className="comments-container">
+                <div className="card">
+                    <div className="info-user">
+                        <div className="img-container">
+                            <img src="images/profile.png" alt="profile"/>
+                        </div>
+                        <div className="user-name">
+                            <h4>IALY Francisco</h4>
+                            <h5>Développeur</h5>
+                        </div>
+                    </div>
+                    <div className="message">
+                        <p>Le service qu'offre LUMINI School est très utile pour les habitants de Madagascar surtout ceux de Toamasina car la plupart des étudiants, jeunes chercheurs en informatique sont en manque de cadre pédagogique.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="info-user">
+                        <div className="img-container">
+                            <img src="images/profile.png" alt="profile"/>
+                        </div>
+                        <div>
+                            <h5>IALY Francisco</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="info-user">
+                        <div className="img-container">
+                            <img src="images/profile.png" alt="profile"/>
+                        </div>
+                        <div className="user-name">
+                            <h4>Sederaniaina R.</h4>
+                            <h5>A. en appel sortant</h5>
+                        </div>
+                    </div>
+                    <div className="message">
+                        <p>Il est temps que tous les jeunes, adultes, enfants de nos povinces soient beaucoup familiarisés au monde de l'informatique. Ainsi ils apprennent les grands moyens afin de réaliser des grandes idées.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="info-user">
+                        <div className="img-container">
+                            <img src="images/profile.png" alt="profile"/>
+                        </div>
+                        <div>
+                            <h5>IALY Francisco</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="info-user">
+                        <div className="img-container">
+                            <img src="images/profile.png" alt="profile"/>
+                        </div>
+                        <div className="user-name">
+                            <h4>Franciska Zoé</h4>
+                            <h5>Etudiante en info.</h5>
+                        </div>
+                    </div>
+                    <div className="message">
+                        <p>Je n'ai jamais su que se former en informatique peut être si facile que je le croiais. Mais grâce à LUMINI School tout s'est dévenu possible et je comprends mieux que l'informatique est une base fiable. "Big thx 👌" à leur équipe.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="info-user">
+                        <div className="img-container">
+                            <img src="images/profile.png" alt="profile"/>
+                        </div>
+                        <div>
+                            <h5>IALY Francisco</h5>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </section>
     )
 }
