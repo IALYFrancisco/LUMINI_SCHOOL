632df0fc7be4bce5593d7a952aae08c1e2154e26
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-20 07:31:26 +0300

    section footer intégrée mais pas encore responsivée

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './views/home'
 import Nav from './components/nav'
+import Footer from './components/footer'
 
 function App() {
   return (
     <>
       <Nav></Nav>
       <Home></Home>
+      <Footer></Footer>
     </>
   )
 }
