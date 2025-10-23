0f7112b399a47fd19af47afafd2ed725a7690920
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-23 21:51:57 +0300

    développement des listes des carte des formations

--- src/components/formations.jsx
+++ src/components/formations.jsx
@@ -6,6 +6,19 @@ export function Formations(){
                     <h2>Jetez un coup d'oeil à nos formations</h2>
                     <p>On propose aux apprentis des modules de formation de haute qualité avec des supports, fruits de prudence et d'amour. Nos formations sont variées sur tout les secteurs de l'informatique.</p>
                 </div>
+                <div className="cards-container">
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                </div>
             </section>
         </section>
     )
