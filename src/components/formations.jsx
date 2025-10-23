217d49fb63c525485059411ce95f51d9af16af97
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-23 22:20:56 +0300

    toujours dans le développement de la section démo liste des formations

--- src/components/formations.jsx
+++ src/components/formations.jsx
@@ -7,18 +7,58 @@ export function Formations(){
                     <p>On propose aux apprentis des modules de formation de haute qualité avec des supports, fruits de prudence et d'amour. Nos formations sont variées sur tout les secteurs de l'informatique.</p>
                 </div>
                 <div className="cards-container">
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
-                    <div className="card"></div>
+                    <div className="card">
+                        <div className="formation-image">
+                            <img src="images/dietetique.webp" alt="" />
+                        </div>
+                        <div className="formation-infos">
+                            <h4>Bases du HTML et CSS</h4>
+                            <p>Cette formation apprend les bases du HTML et CSS dans le cadre de création de siteweb.</p>
+                            <button>S'inscrire</button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="formation-image">
+                            <img src="images/dietetique.webp" alt="" />
+                        </div>
+                        <div className="formation-infos">
+                            <h4>Bases du HTML et CSS</h4>
+                            <p>Cette formation apprend les bases du HTML et CSS dans le cadre de création de siteweb.</p>
+                            <button>S'inscrire</button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="formation-image">
+                            <img src="images/dietetique.webp" alt="" />
+                        </div>
+                        <div className="formation-infos">
+                            <h4>Bases du HTML et CSS</h4>
+                            <p>Cette formation apprend les bases du HTML et CSS dans le cadre de création de siteweb.</p>
+                            <button>S'inscrire</button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="formation-image">
+                            <img src="images/dietetique.webp" alt="" />
+                        </div>
+                        <div className="formation-infos">
+                            <h4>Bases du HTML et CSS</h4>
+                            <p>Cette formation apprend les bases du HTML et CSS dans le cadre de création de siteweb.</p>
+                            <button>S'inscrire</button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="formation-image">
+                            <img src="images/dietetique.webp" alt="" />
+                        </div>
+                        <div className="formation-infos">
+                            <h4>Bases du HTML et CSS</h4>
+                            <p>Cette formation apprend les bases du HTML et CSS dans le cadre de création de siteweb.</p>
+                            <button>S'inscrire</button>
+                        </div>
+                    </div>
                 </div>
+                <button>Voir toute les formations</button>
             </section>
         </section>
     )
