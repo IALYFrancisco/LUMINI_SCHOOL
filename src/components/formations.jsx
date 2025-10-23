e5410ed67220aa70c8328861507326a7f346cee5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-23 21:32:33 +0300

    développement encore de la section des démos de formation

--- src/components/formations.jsx
+++ src/components/formations.jsx
@@ -1,5 +1,12 @@
 export function Formations(){
     return(
-        <h1>This is formations components</h1>
+        <section>
+            <section className="formations-container">
+                <div className="head">
+                    <h2>Jetez un coup d'oeil à nos formations</h2>
+                    <p>On propose aux apprentis des modules de formation de haute qualité avec des supports, fruits de prudence et d'amour. Nos formations sont variées sur tout les secteurs de l'informatique.</p>
+                </div>
+            </section>
+        </section>
     )
 }
