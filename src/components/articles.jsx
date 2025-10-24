2f5e39138e0b374138c499d827e22ffff577c1c1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-24 07:54:54 +0300

    développement encore de la section extrait d'article

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -2,10 +2,54 @@ export function Articles(){
     return(
         <section className="blog-container">
             <section>
-                <div className="left"></div>
+                <div className="left">
+                    <div className="cards-container">
+                        <div className="card">
+                            <div className="blog-image">
+                                <img src="images/ergotherapie.webp" alt="" />
+                            </div>
+                            <div className="blog-infos">
+                                <h4>Histoire du numérique</h4>
+                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                                <button>S'inscrire</button>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="blog-image">
+                                <img src="images/ergotherapie.webp" alt="" />
+                            </div>
+                            <div className="blog-infos">
+                                <h4>Histoire du numérique</h4>
+                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                                <button>S'inscrire</button>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="blog-image">
+                                <img src="images/ergotherapie.webp" alt="" />
+                            </div>
+                            <div className="blog-infos">
+                                <h4>Histoire du numérique</h4>
+                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                                <button>S'inscrire</button>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="blog-image">
+                                <img src="images/ergotherapie.webp" alt="" />
+                            </div>
+                            <div className="blog-infos">
+                                <h4>Histoire du numérique</h4>
+                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                                <button>S'inscrire</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="right">
                     <h2>Mettez-vous à nos places juste un instant</h2>
-                    <p></p>
+                    <p>On partage aussi au monde entier notre point de vue vis à vis d'un sujet. On parle à travers nos articles des événements passés 🌿, nos perceptions du présent 🍃 et nos visions du futur 🍂.</p>
+                    <button>Voir touts les articles</button>
                 </div>
             </section>
         </section>
