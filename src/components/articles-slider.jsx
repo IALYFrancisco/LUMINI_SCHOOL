d293140c2813a5545cf26a45491dfdf32e8fd490
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-25 14:04:16 +0300

    ajout de slide dans la section extraits de blog

---
+++ src/components/articles-slider.jsx
@@ -0,0 +1,100 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css'
+import { useRef } from "react";
+import { Autoplay, Navigation } from "swiper/modules";
+
+export function ArticlesSlider() {
+    const swiperRef = useRef()
+    return(
+        <>
+            <div className="prev" onClick={()=> swiperRef.current?.slideNext()}>
+                <img src="images/icon.png" alt="" />
+            </div>
+            <div className="next" onClick={()=> swiperRef.current?.slidePrev()}>
+                <img src="images/icon.png" alt="" />
+            </div>
+            <Swiper
+                autoplay={{ delay: 3000, disableOnInteraction: false }}
+                loop={true}
+                pagination={{ clickable: true }}
+                spaceBetween={30}
+                slidesPerView={3}
+                onSwiper={(swiper)=> (swiperRef.current = swiper)}
+                modules={[Navigation, Autoplay]}
+            >
+                <SwiperSlide>
+                    <div className="card">
+                        <div className="blog-image">
+                            <img src="images/ergotherapie.webp" alt="" />
+                        </div>
+                        <div className="blog-infos">
+                            <h4>Histoire du numérique</h4>
+                            <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                            <button>Lire plus</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card">
+                        <div className="blog-image">
+                            <img src="images/ergotherapie.webp" alt="" />
+                        </div>
+                        <div className="blog-infos">
+                            <h4>Histoire du numérique</h4>
+                            <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                            <button>Lire plus</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card">
+                        <div className="blog-image">
+                            <img src="images/ergotherapie.webp" alt="" />
+                        </div>
+                        <div className="blog-infos">
+                            <h4>Histoire du numérique</h4>
+                            <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                            <button>Lire plus</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card">
+                        <div className="blog-image">
+                            <img src="images/ergotherapie.webp" alt="" />
+                        </div>
+                        <div className="blog-infos">
+                            <h4>Histoire du numérique</h4>
+                            <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                            <button>Lire plus</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card">
+                        <div className="blog-image">
+                            <img src="images/ergotherapie.webp" alt="" />
+                        </div>
+                        <div className="blog-infos">
+                            <h4>Histoire du numérique</h4>
+                            <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                            <button>Lire plus</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="card">
+                        <div className="blog-image">
+                            <img src="images/ergotherapie.webp" alt="" />
+                        </div>
+                        <div className="blog-infos">
+                            <h4>Histoire du numérique</h4>
+                            <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
+                            <button>Lire plus</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </>
+    )
+}
--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,55 +1,12 @@
+import { ArticlesSlider } from "./articles-slider"
+
 export function Articles(){
     return(
         <section className="blog-container">
             <section>
                 <div className="left">
-                    <div className="prev">
-                        <img src="images/icon.png" alt="" />
-                    </div>
-                    <div className="next">
-                        <img src="images/icon.png" alt="" />
-                    </div>
                     <div className="cards-container">
-                        <div className="card">
-                            <div className="blog-image">
-                                <img src="images/ergotherapie.webp" alt="" />
-                            </div>
-                            <div className="blog-infos">
-                                <h4>Histoire du numérique</h4>
-                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
-                                <button>Lire plus</button>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="blog-image">
-                                <img src="images/ergotherapie.webp" alt="" />
-                            </div>
-                            <div className="blog-infos">
-                                <h4>Histoire du numérique</h4>
-                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
-                                <button>Lire plus</button>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="blog-image">
-                                <img src="images/ergotherapie.webp" alt="" />
-                            </div>
-                            <div className="blog-infos">
-                                <h4>Histoire du numérique</h4>
-                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
-                                <button>Lire plus</button>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="blog-image">
-                                <img src="images/ergotherapie.webp" alt="" />
-                            </div>
-                            <div className="blog-infos">
-                                <h4>Histoire du numérique</h4>
-                                <p>A l'époque où la digitalisation n'était pas encore née, les gens utilisent des machines de frappe.</p>
-                                <button>Lire plus</button>
-                            </div>
-                        </div>
+                        <ArticlesSlider></ArticlesSlider>
                     </div>
                 </div>
                 <div className="right">
