7c2a704d1d49d5cf251385b488f1137ef60e436c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-25 06:56:43 +0300

    création et configuration des routes pour la vue de creation de compte

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './views/home'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Login } from './views/login'
+import { Register } from './views/register'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/authentication/login' element={<Login/>}></Route>
+        <Route path='/authentication/register' element={<Register/>}></Route>
       </Routes>
     </Router>
   )
