19bbe8b7cdedc22627a043068a587274c59ac8d1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-26 14:46:50 +0300

    développement de la section formations dans le tableau de bord

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,12 +1,12 @@
 import Sidebar from "../../components/dashboard/sidebar"
 import '../../../public/styles/dashboard/dashboard.css'
-
+import Formations from "../../components/dashboard/formations"
 export default function Dashboard(){
     return(
         <section id="dashboard-view">
             <Sidebar></Sidebar>
-            <section>
-                
+            <section className="sections-container">
+                <Formations></Formations>
             </section>
         </section>
     )
