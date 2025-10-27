3a756e85b83461fdb66f807cb02655be4b9858b4
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-27 15:05:59 +0300

    développement de la liste de formation dans le dashboard

--- src/components/dashboard/formations.jsx
+++ src/components/dashboard/formations.jsx
@@ -6,6 +6,89 @@ export default function Formations(){
                 <input type="text" name="" id="" placeholder="Recherche de formation"/>
                 <button>Ajouter une formation</button>
             </div>
+            <ul className="formations">
+                <li className="titles">
+                    <ul>
+                        <li className="title">Titres</li>
+                        <li className="description">Descriptions</li>
+                        <li className="addDate">Date d'ajout</li>
+                        <li className="publicationDate">Date de publication</li>
+                        <li className="published">Publiée</li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="formation">
+                        <li className="title">
+                            <h5>Bases du HTML et CSS</h5>
+                        </li>
+                        <li  className="description">
+                            <p>Cette formation apprend aux débutants les bases du HTML et CSS.</p>
+                        </li>
+                        <li  className="addDate">
+                            <p>27 Octobre 2025 à 16:49</p>
+                        </li>
+                        <li className="publicationDate">
+                            <p>29 Octobre 2025 à 8:12</p>
+                        </li>
+                        <li className="published">
+                            <div className="badge">
+                                <p>oui</p>
+                            </div>
+                            {/* <div className="badge">
+                                <p>non</p>
+                            </div> */}
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="formation">
+                        <li className="title">
+                            <h5>Bases du HTML et CSS</h5>
+                        </li>
+                        <li  className="description">
+                            <p>Cette formation apprend aux débutants les bases du HTML et CSS.</p>
+                        </li>
+                        <li  className="addDate">
+                            <p>27 Octobre 2025 à 16:49</p>
+                        </li>
+                        <li className="publicationDate">
+                            <p>29 Octobre 2025 à 8:12</p>
+                        </li>
+                        <li className="published">
+                            <div className="badge">
+                                <p>oui</p>
+                            </div>
+                            {/* <div className="badge">
+                                <p>non</p>
+                            </div> */}
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <ul className="formation">
+                        <li className="title">
+                            <h5>Bases du HTML et CSS</h5>
+                        </li>
+                        <li  className="description">
+                            <p>Cette formation apprend aux débutants les bases du HTML et CSS.</p>
+                        </li>
+                        <li  className="addDate">
+                            <p>27 Octobre 2025 à 16:49</p>
+                        </li>
+                        <li className="publicationDate">
+                            <p>29 Octobre 2025 à 8:12</p>
+                        </li>
+                        <li className="published">
+                            <div className="badge">
+                                <p>oui</p>
+                            </div>
+                            {/* <div className="badge">
+                                <p>non</p>
+                            </div> */}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </>
     )
 }
