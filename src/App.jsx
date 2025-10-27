99ba001963c7258768f47a878c135c6819f0c535
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-27 19:48:04 +0300

    configuration des routes enfants dans l'application

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Login } from './views/login'
 import { Register } from './views/register'
 import Dashboard from './views/dashboard'
+import Settings from './components/dashboard/settings'
+import Formations from './components/dashboard/formations'
+import Inscriptions from './components/dashboard/inscriptions'
 
 function App() {
   return (
@@ -12,7 +15,11 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/authentication/login' element={<Login/>}></Route>
         <Route path='/authentication/register' element={<Register/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}>
+          <Route index element={<Formations/>}/>
+          <Route path='inscriptions' element={<Inscriptions/>}/>
+          {/* <Route path='settings' element={<Settings/>}/> */}
+        </Route>
       </Routes>
     </Router>
   )
