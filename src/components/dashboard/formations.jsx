35fe5153af96cd97e508f0a57a95277fc8345266
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-27 07:55:12 +0300

    ajout des actions statiques dans la page dashboard (recherche de formation et ajout d'une formation)

--- src/components/dashboard/formations.jsx
+++ src/components/dashboard/formations.jsx
@@ -1,5 +1,11 @@
 export default function Formations(){
     return(
-        <h2>Formations</h2>
+        <>
+            <h2>Formations</h2>
+            <div className="actions">
+                <input type="text" name="" id="" placeholder="Recherche de formation"/>
+                <button>Ajouter une formation</button>
+            </div>
+        </>
     )
 }
