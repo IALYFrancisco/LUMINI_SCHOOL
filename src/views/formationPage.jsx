cf3f5a6c57d0da3c09c0a246209a8bc948e7e10a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-10 13:53:40 +0300

    développement de la page liste de toute les formations

--- src/views/formationPage.jsx
+++ src/views/formationPage.jsx
@@ -7,8 +7,11 @@ export function FormationsPage(){
             <Nav></Nav>
             <section className="formations-page">
                 <div className="head">
-                    <h2>Toute nos formations</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione quae sapiente voluptas sequi consequuntur atque recusandae rem praesentium. Nisi laborum nesciunt alias sequi et autem inventore magnam doloremque enim voluptatem?</p>
+                    <h2>Toute nos formations :</h2>
+                    <p>Ci-dessous la liste de toute nos formations. Elles sont issues des branches existantes du secteur de l'informatique et ont été éléborées par nous-même afin de garantir leurs contenus ✨.</p>
+                    <div className="actions">
+                        <input type="text" name="formation" id="" placeholder="Rehcrecher des formations"/>
+                    </div>
                 </div>
             </section>
         </>
