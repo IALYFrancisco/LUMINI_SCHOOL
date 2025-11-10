f1e8c8caca6cc4aee009cd6bf80e4cf1f2a846d7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-10 14:29:56 +0300

    ajout d'un peu de changement à la barre de recherche de formation

--- src/views/formationPage.jsx
+++ src/views/formationPage.jsx
@@ -42,6 +42,20 @@ export function FormationsPage(){
                             </div>
                         ))}
                     </>}
+                    { formations && <>
+                        { formations.map( formation => (
+                            <div className="card" key={formation._id}>
+                                <div className="formation-image">
+                                    <img src={`${import.meta.env.VITE_API_BASE_URL}/${formation.image}`} alt="" />
+                                </div>
+                                <div className="formation-infos">
+                                    <h4>{formation.title}</h4>
+                                    <p>{formation.description}</p>
+                                    <button>S'inscrire</button>
+                                </div>
+                            </div>
+                        ))}
+                    </>}
                 </div>
             </section>
         </>
