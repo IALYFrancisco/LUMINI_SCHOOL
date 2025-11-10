340782654d1452aa36435916bdac821bf3b6ff88
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-10 13:24:10 +0300

    développement de la page de liste de toute les formations

--- src/views/formationPage.jsx
+++ src/views/formationPage.jsx
@@ -1,10 +1,16 @@
 import Nav from "../components/nav"
+import '../../public/styles/formationsPage.css'
 
 export function FormationsPage(){
     return(
         <>
             <Nav></Nav>
-            <h1>All formations list page.</h1>
+            <section className="formations-page">
+                <div className="head">
+                    <h2>Toute nos formations</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione quae sapiente voluptas sequi consequuntur atque recusandae rem praesentium. Nisi laborum nesciunt alias sequi et autem inventore magnam doloremque enim voluptatem?</p>
+                </div>
+            </section>
         </>
     )
 }
