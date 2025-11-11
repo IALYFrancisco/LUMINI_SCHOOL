7be7596350ba5e3f37313b9cf0e21d7fc7e98206
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-11 14:05:05 +0300

    développement de la page d'inscription à une formation

--- src/views/registrations.jsx
+++ src/views/registrations.jsx
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom"
+import Nav from "../components/nav"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export default function Registrations(){
 
+    var [formation, setFormation] = useState(null)
+    var [loading, setLoading] = useState(true)
     var { id } = useParams()
 
+    useEffect(()=>{
+        axios
+    })
+
     return(
-        <h1>Registration page {id}</h1>
+        <>
+            <Nav></Nav>
+            <section className="registrations-container">
+                <h2>Inscription à la formation {}</h2>
+            </section>
+        </>
     )
 }
