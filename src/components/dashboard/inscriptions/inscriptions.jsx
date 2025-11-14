748637a4c8f0328d2b5fb38c6661769845ce1fd8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-14 09:38:39 +0300

    développement de la liste des inscriptions

--- src/components/dashboard/inscriptions/inscriptions.jsx
+++ src/components/dashboard/inscriptions/inscriptions.jsx
@@ -1,5 +1,22 @@
 export default function Inscriptions(){
     return(
-        <h1>This is the view of inscription page</h1>
+        <>
+            <h2>Inscriptions</h2>
+            <div className="actions">
+                <input type="text" name="" id="" placeholder="Recherche d'une inscription"/>
+            </div>
+            <ul className="formations">
+                <li className="titles">
+                    <ul>
+                        <li className="title">Titres</li>
+                        <li className="description">Descriptions</li>
+                        <li className="addDate">Date d'ajout</li>
+                        <li className="publicationDate">Date de publication</li>
+                        <li className="published">Publiée</li>
+                        <li className="formation-actions">Actions</li>
+                    </ul>
+                </li>
+            </ul>
+        </>
     )
 }
