2143aa3dbbbf43f1ed95cbe4542778ef43c6f0fd
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-28 14:15:04 +0300

    résolution de bug d'inscription d'utilisateur sur une formation

--- src/views/registrations.jsx
+++ src/views/registrations.jsx
@@ -49,10 +49,7 @@ export default function Registrations(){
 
     useEffect(()=>{
         axios.get(`${import.meta.env.VITE_API_BASE_URL}/formation/get?_id=${id}`)
-        .then((response)=>{
-            setFormation(response.data);
-            reset()
-        })
+        .then((response)=>setFormation(response.data))
         .catch(()=>setFormation(null))
         .finally(()=>setLoading(false))
         axios.get(`${import.meta.env.VITE_API_BASE_URL}/user/informations`, {withCredentials: true})
@@ -101,7 +98,7 @@ export default function Registrations(){
                                         </div>
                                         <div className="element">
                                             <label htmlFor="">Votre numéro téléphone <span className="colored">*</span> :</label>
-                                            <input type="tel" name="" id="" placeholder="ex: 030 00 000 00" { ...register('phoneNumber', { required:true }) } required />
+                                            <input type="tel" name="" id="" placeholder="ex: 030 00 000 00" { ...register('phoneNumber', { required:true, value: user.phoneNumber ? user.phoneNumber : "" }) } required />
                                         </div>
                                     </fieldset>
                                 </div>
