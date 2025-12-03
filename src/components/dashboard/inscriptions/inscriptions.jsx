4399fc847c7dc51ef9e23814834f1c479ec2736c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-03 10:09:08 +0300

    développment de la liste de formation dans le tableau de bord

--- src/components/dashboard/inscriptions/inscriptions.jsx
+++ src/components/dashboard/inscriptions/inscriptions.jsx
@@ -20,9 +20,7 @@ export default function Inscriptions(){
                 <li className="titles">
                     <ul>
                         <li className="title">Titres du formation</li>
-                        <li className="description">Clients inscrits</li>
-                        <li className="addDate">Date de l'inscription</li>
-                        <li className="formation-actions">Actions</li>
+                        <li className="clients-subscribed">Nombre de clients inscrits</li>
                     </ul>
                 </li>
                 { registrations && <li>
@@ -31,25 +29,9 @@ export default function Inscriptions(){
                                     <li className="title">
                                         <h5>{registration.formation.title}</h5>
                                     </li>
-                                    <li  className="description">
-                                        <p>{registration.user.name}</p>
+                                    <li  className="clients-subscribed">
+                                        <p>23</p>
                                     </li>
-                                    <li  className="addDate">
-                                        <p>{ new Date(registration.registrationDate).toLocaleString("fr-FR") }</p>
-                                    </li>
-                                    {/* <li className="formation-actions">
-                                        <ul className={ activePopUp === formation._id ? 'pop-up show' : 'pop-up hide'}>
-                                            <li onClick={ () => {
-                                                togglePopUp(formation._id);
-                                                deleteFormation(formation._id);
-                                            }} >Supprimer</li>
-                                            <li onClick={ () => togglePopUp(formation._id) }>Publier</li>
-                                            <li onClick={ () => togglePopUp(formation._id) }>Modifier</li>
-                                        </ul>
-                                        <div className="custom-container" onClick={ () => togglePopUp(formation._id) }>
-                                            <img src="/images/kebab.png" alt=""/>
-                                        </div>
-                                    </li> */}
                                 </ul>
                                 )
                             )
