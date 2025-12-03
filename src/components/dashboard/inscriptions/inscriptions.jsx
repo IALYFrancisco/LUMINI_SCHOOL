80663becafd835a87a3cbd33cdb6770cb8622404
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-03 16:36:38 +0300

    suppression de codes commentés il y a longtemps

--- src/components/dashboard/inscriptions/inscriptions.jsx
+++ src/components/dashboard/inscriptions/inscriptions.jsx
@@ -37,19 +37,6 @@ export default function Inscriptions(){
                                     <li  className="addDate">
                                         <p>{ new Date(registration.registrationDate).toLocaleString("fr-FR") }</p>
                                     </li>
-                                    {/* <li className="formation-actions">
-                                        <ul className={ activePopUp === formation._id ? 'pop-up show' : 'pop-up hide'}>
-                                            <li onClick={ () => {
-                                                togglePopUp(formation._id);
-                                                deleteFormation(formation._id);
-                                            }} >Supprimer</li>
-                                            <li onClick={ () => togglePopUp(formation._id) }>Publier</li>
-                                            <li onClick={ () => togglePopUp(formation._id) }>Modifier</li>
-                                        </ul>
-                                        <div className="custom-container" onClick={ () => togglePopUp(formation._id) }>
-                                            <img src="/images/kebab.png" alt=""/>
-                                        </div>
-                                    </li> */}
                                 </ul>
                                 )
                             )
