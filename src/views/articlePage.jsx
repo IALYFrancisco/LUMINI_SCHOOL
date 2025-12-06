3d4403573458adcb6f74be80e36e06c1c5f2095d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-06 18:21:31 +0300

    développement de la page liste de tout articles publiés

---
+++ src/views/articlePage.jsx
@@ -0,0 +1,57 @@
+import Nav from "../components/nav"
+import '../../public/styles/formationsPage.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+import Loading from "../components/loading"
+
+export function FormationsPage(){
+
+    var [ formations, setFormations ] = useState([])
+    var [ loading, setLoading ] = useState(true)
+
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/formation/get`, { withCredentials: true })
+            .then((response)=>{
+                setFormations(response.data.filter( f => f.published === true ))
+            })
+            .catch(()=>setFormations([]))
+            .finally(()=>setLoading(false))
+    }, [])
+
+    if(loading) return <Loading/>
+    if(formations) return(
+        <>
+            <Nav></Nav>
+            <section className="formations-page">
+                <div className="head">
+                    <h2>Toute nos formations :</h2>
+                    <p>Ci-dessous la liste de toute nos formations. Elles sont issues des branches existantes du secteur de l'informatique et ont été éléborées par nous-même afin de garantir leurs contenus ✨.</p>
+                    <div className="actions">
+                        <input type="text" name="formation" id="" placeholder="Rehcrecher des formations"/>
+                    </div>
+                </div>
+                <div className="body">
+                    { formations && <>
+                        { formations.map( formation => (
+                            <div className="card-container" key={formation._id}>
+                                <div className="card">
+                                    <div className="formation-image">
+                                        <img src={ (formation.image.includes('https') || formation.image.includes('http')) ? formation.image : `${import.meta.env.VITE_API_BASE_URL}/${formation.image}` } alt="" />
+                                    </div>
+                                    <div className="formation-infos">
+                                        <h4>{formation.title}</h4>
+                                        <p>{formation.description}</p>
+                                        <Link to={`/registrations/formation/${formation._id}`}>
+                                            <button>S'inscrire</button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </> }
+                </div>
+            </section>
+        </>
+    )
+}
