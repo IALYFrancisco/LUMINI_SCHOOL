25628939b934a9aac142763d9180343790821ad1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-08 10:09:01 +0300

    développement de la page de modification des informations personnelles

--- src/components/dashboard/settings/index.jsx
+++ src/components/dashboard/settings/index.jsx
@@ -1,11 +1,20 @@
 import { useEffect } from 'react'
 import '../../../../public/styles/dashboard/setting.css'
 import { useAuth } from '../../../contexts/AuthContext'
-import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 export default function Settings(){
 
     const { user } = useAuth()
+    const { reset, register } = useForm()
+
+    useEffect(()=>{
+        reset({
+            name: user.name,
+            email: user.email,
+            phoneNumber: user.phoneNumber
+        })
+    }, [reset, user])
 
     return(
         <>
@@ -21,11 +30,11 @@ export default function Settings(){
                             <fieldset>
                                 <div className="element">
                                     <label>Nom d'utilisateur :</label>
-                                    <input type="text" name="" id="" placeholder="Votre nom complet"/>
+                                    <input type="text" name="" id="" placeholder="Votre nom complet" { ...register('name') }/>
                                 </div>
                                 <div className="element">
                                     <label>Votre email :</label>
-                                    <input type="email" name="" id="" placeholder="Ex: johndoe@example.com"/>
+                                    <input type="email" name="" id="" placeholder="Ex: johndoe@example.com" { ...register('email') }/>
                                 </div>
                                 <div className="element">
                                     <label>Votre image de profile :</label>
@@ -34,7 +43,7 @@ export default function Settings(){
                                 </div>
                                 <div className="element">
                                     <label>Votre numéro téléphone :</label>
-                                    <input type="tel" name="" id="" placeholder='Ex: 030 00 000 00'/>
+                                    <input type="tel" name="" id="" placeholder='Ex: 030 00 000 00' { ...register('phoneNumber') }/>
                                 </div>
                             </fieldset>
                         </form>
