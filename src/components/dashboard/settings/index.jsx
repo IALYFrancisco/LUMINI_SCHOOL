8f87cd87b7cfde173b90d7fc93fb207ae176ec1a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-08 09:18:53 +0300

    développement de la page de parmètre

--- src/components/dashboard/settings/index.jsx
+++ src/components/dashboard/settings/index.jsx
@@ -15,15 +15,20 @@ export default function Settings(){
                             <fieldset>
                                 <div className="element">
                                     <label>Nom d'utilisateur :</label>
-                                    <input type="text" name="" id="" />
+                                    <input type="text" name="" id="" placeholder="Votre nom complet"/>
                                 </div>
                                 <div className="element">
                                     <label>Votre email :</label>
-                                    <input type="email" name="" id="" />
+                                    <input type="email" name="" id="" placeholder="Ex: johndoe@example.com"/>
+                                </div>
+                                <div className="element">
+                                    <label>Votre image de profile :</label>
+                                    <input type="url" name="" id="" placeholder="Utilisez cet champ pour une image déjà en ligne"/>
+                                    <input type="file" name="" id="" />
                                 </div>
                                 <div className="element">
                                     <label>Votre numéro téléphone :</label>
-                                    <input type="tel" name="" id="" />
+                                    <input type="tel" name="" id="" placeholder='Ex: 030 00 000 00'/>
                                 </div>
                             </fieldset>
                         </form>
