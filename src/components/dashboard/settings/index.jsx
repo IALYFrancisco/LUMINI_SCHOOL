e30f36c57e4d6965e0392ccf23afe30303260b55
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-08 17:22:16 +0300

    développement encore de la page de modification d'information d'utilisateur

--- src/components/dashboard/settings/index.jsx
+++ src/components/dashboard/settings/index.jsx
@@ -6,11 +6,25 @@ import { useForm } from 'react-hook-form'
 export default function Settings(){
 
     const { user } = useAuth()
-    const { reset, register } = useForm()
+    
+    const { reset, register, watch } = useForm()
     var [ infoFormActive, setInfoFormActive ] = useState(false)
+    var [ imageIsDefined, setImageIsDefined ] = useState(false)
+    var [ urlIsDefined, setUrlIsDefined ] = useState(false)
+    var [ image, setImage ] = useState('')
+
+    var watchAll = watch()
 
     const toogleInfoForm = ()=>{
         infoFormActive ? setInfoFormActive(false) : setInfoFormActive(true)
+        if(infoFormActive){
+            reset({
+                name: user.name,
+                email: user.email,
+                profile: (user.profile.includes('https') || user.profile.includes('http')) ? user.profile : `${import.meta.env.VITE_API_BASE_URL}${user.profile}`,
+                phoneNumber: user.phoneNumber
+            })
+        }
     }
 
     useEffect(()=>{
@@ -21,6 +35,16 @@ export default function Settings(){
             phoneNumber: user.phoneNumber
         })
     }, [reset, user])
+
+    useEffect(()=>{
+
+        if(image) setImageIsDefined(true)
+        else setImageIsDefined(false)
+
+        if(watchAll.profile) setUrlIsDefined(true)
+        else setImageIsDefined(false)
+        
+    }, [image, watchAll.profile])
 
     return(
         <>
@@ -39,20 +63,20 @@ export default function Settings(){
                                 </div>
                                 <div className="element">
                                     <label>Nom d'utilisateur :</label>
-                                    <input type="text" name="" id="" placeholder="Votre nom complet" { ...register('name') }/>
+                                    <input type="text" name="" id="name" placeholder="Votre nom complet" { ...register('name') }/>
                                 </div>
                                 <div className="element">
                                     <label>Votre email :</label>
-                                    <input type="email" name="" id="" placeholder="Ex: johndoe@example.com" { ...register('email') }/>
+                                    <input type="email" name="" id="email" placeholder="Ex: johndoe@example.com" { ...register('email') }/>
                                 </div>
                                 <div className="element">
                                     <label>Votre image de profile :</label>
-                                    <input type="url" name="" id="" placeholder="Utilisez cet champ pour une image déjà en ligne" { ...register('profile') }/>
-                                    <input type="file" name="" id="" accept="image/jpeg, image/png"/>
+                                    <input disabled={imageIsDefined} type="url" name="" id="profile_url" placeholder="Utilisez cet champ pour une image déjà en ligne" { ...register('profile') } onChange={()=>{console.log(urlIsDefined), console.log(watchAll.profile)}}/>
+                                    <input disabled={urlIsDefined} type="file" name="" id="profile_file" accept="image/jpeg, image/png" onChange={ (e)=>setImage(e.target.files[0]) }/>
                                 </div>
                                 <div className="element">
                                     <label>Votre numéro téléphone :</label>
-                                    <input type="tel" name="" id="" placeholder='Ex: 030 00 000 00' { ...register('phoneNumber') }/>
+                                    <input type="tel" name="" id="telephone" placeholder='Ex: 030 00 000 00' { ...register('phoneNumber') }/>
                                 </div>
                                 <div className="element">
                                     <button>Soumettre</button>
