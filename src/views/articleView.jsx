68b2ef40ffb544262ad67008515fd64e4a3faaca
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-10 09:42:30 +0300

    développement de la page de lecture d'article

--- src/views/articleView.jsx
+++ src/views/articleView.jsx
@@ -1,8 +1,32 @@
 import { useParams } from "react-router-dom"
+import Nav from "../components/nav"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Loading from "../components/loading"
 
 export default function ArticleView(){
 
     const { id } = useParams()
+    var [ article, setArticle ] = useState(null)
+    var [ loading, setLoading ] = useState(true)
 
-    return <h1>This is an article view page: {id}</h1>
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/article/get?_id=${id}`)
+        .then((response)=>{
+            setArticle(response.data)
+        }).finally(()=>setLoading(false))
+    }, [id])
+
+    if (loading) return <Loading/>
+    return (
+        <>
+            <Nav></Nav>
+            {
+                article && <div className="image-container">
+                <img src={(article.image.includes('https') || article.image.includes('http')) ? article.image : `${import.meta.env.VITE_API_BASE_URL}/${article.image}` } alt={article.title} />
+            </div>
+            }
+            
+        </>
+    )
 }
