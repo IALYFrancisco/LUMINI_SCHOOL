0d55e82e03a6338ecf7d2310c2a5781e07155bc2
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-11 10:08:05 +0300

    création de composant protection isAdmin

---
+++ src/components/isAdmin.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import Loading from "./loading";
+
+export default function IsSuperuser({children}) {
+    const { user, loading } = useAuth()
+    if(loading) return <Loading/>
+    if(user && user.status === "admin") return children
+    return <Navigate to="/" replace/>
+}
