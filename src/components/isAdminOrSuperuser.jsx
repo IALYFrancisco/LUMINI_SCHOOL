0111ea0615594d56d86ec7e3077a216bbf316f79
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-11 10:11:07 +0300

    abadon des ces changements

--- src/components/isAdminOrSuperuser.jsx
+++ src/components/isAdminOrSuperuser.jsx
@@ -2,9 +2,11 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import Loading from "./loading";
 
-export default function IsSuperuser({children}) {
+export default function IsAdminOrSuperuser({children}) {
     const { user, loading } = useAuth()
+
     if(loading) return <Loading/>
-    if(user && user.status === "admin") return children
+    if(user && (user.status === "admin" || user.status === "superuser")) return children
     return <Navigate to="/" replace/>
+
 }
