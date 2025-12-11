5754067406293ff6bfc35e3497a6dab28c3a1a58
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-11 19:00:27 +0300

    mis à jour de l'interface d'ajout de formation

--- src/components/dashboard/formations/addFormations.jsx
+++ src/components/dashboard/formations/addFormations.jsx
@@ -60,40 +60,66 @@ export default function AddFormation(){
             <section className="add-formation-form">
                 <h3>Ajout d'une formation :</h3>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <fieldset>
-                        <div className="element">
-                            <label>Titre de la formation :</label>
-                            <input type="text" required name="titre" placeholder="Ajoutez un titre pour la formation" { ...register("title", { required: true })}/>
-                        </div>
-                        <div className="element">
-                            <label>Image de mis en avant pour la formation :</label>
-                            <input disabled={ urlIsDefined } type="file" name="image" id="" required accept="image/jpeg, image/png" onChange={(e) => setImage(e.target.files[0])}/>
-                            <input disabled={ imageIsDefined } type="url" name="image" id="" { ...register("url") } placeholder="Utilisez cet champ pour une image en ligne" />
-                        </div>
-                        <div className="element">
-                            <label>Les prérequis du formation :</label>
-                            <input type="text" name="prerequis" id="" placeholder="Doivent être séparés par un point-virgule" { ...register("prerequisites", {required: true}) } required />
-                        </div>
-                        <div className="element">
-                            <button>Soumettre</button>
-                        </div>
-                    </fieldset>
-                    <fieldset>
-                        <div className="element">
-                            <label>Descriptions de la formation : <p>nombre de mots : {wordCount} / 150</p></label>
-                            <textarea cols="30" rows="10" required name="descriptions" placeholder="Redigez ici les descriptions ..." { ...register("description", { required: "La description est obligatoire.", validate: {
-                                minWords: (value) => 
-                                    value.trim().split(/\s+/).length >= 50 ||
-                                "La description doit contenir au moins 50 mots.",
-                                maxWords: (value) =>
-                                    value.trim().split(/\s+/).length <= 150 ||
-                                "La description ne doit pas dépasser 150 mots."
-                            } }) } ></textarea>
-                        </div>
-                        { errors.description && (
-                            <p className="message">{errors.description.message}</p>
-                        ) }
-                    </fieldset>
+                    <div className="fieldsets-top">
+                        <fieldset>
+                            <div className="element">
+                                <label>Titre de la formation :</label>
+                                <input type="text" required name="titre" placeholder="Ajoutez un titre pour la formation" { ...register("title", { required: true })}/>
+                            </div>
+                            <div className="element">
+                                <label>Image de mis en avant pour la formation :</label>
+                                <input disabled={ urlIsDefined } type="file" name="image" id="" required accept="image/jpeg, image/png" onChange={(e) => setImage(e.target.files[0])}/>
+                                <input disabled={ imageIsDefined } type="url" name="image" id="" { ...register("url") } placeholder="Utilisez cet champ pour une image en ligne" />
+                            </div>
+                            <div className="element">
+                                <label>Les prérequis du formation :</label>
+                                <input type="text" name="prerequis" id="" placeholder="Doivent être séparés par un point-virgule" { ...register("prerequisites", {required: true}) } required />
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="element">
+                                <label>Descriptions de la formation : <p>nombre de mots : {wordCount} / 150</p></label>
+                                <textarea cols="30" rows="10" required name="descriptions" placeholder="Redigez ici les descriptions ..." { ...register("description", { required: "La description est obligatoire.", validate: {
+                                    minWords: (value) => 
+                                        value.trim().split(/\s+/).length >= 50 ||
+                                    "La description doit contenir au moins 50 mots.",
+                                    maxWords: (value) =>
+                                        value.trim().split(/\s+/).length <= 150 ||
+                                    "La description ne doit pas dépasser 150 mots."
+                                } }) } ></textarea>
+                            </div>
+                            { errors.description && (
+                                <p className="message">{errors.description.message}</p>
+                            ) }
+                        </fieldset>
+                    </div>
+                    <div className="fieldsets-bottom">
+                        <fieldset>
+                            <div className="element">
+                                <label>Date et heure de début du formation :</label>
+                                <input type="datetime-local" required name="beginDate" { ...register("beginDate", { required: true })}/>
+                            </div>
+                            <div className="element">
+                                <label>Date et heure de fin du formation :</label>
+                                <input type="datetime-local" required name="endDate" { ...register("endDate", { required: true }) }/>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="element">
+                                <label>Lieu du formation :</label>
+                                <input type="text" required name="coursePlace" { ...register("coursePlace", { required: true })} placeholder="L'endroit où se déroule la formation"/>
+                            </div>
+                            <div className="element">
+                                <label>Coût de la formation (en Ar) :</label>
+                                <input type="number" min="1" required name="endDate" { ...register("coursePrice", { required: true }) } placeholder="Entrez un montant en ariary"/>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="element">
+                                <button>Soumettre</button>
+                            </div>
+                        </fieldset>
+                    </div>
                 </form>
             </section>
         </>
