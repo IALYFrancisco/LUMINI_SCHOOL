a628fe975d882592ba141765b4f6edc77f9bffdc
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-14 10:46:57 +0300

    développement de la page de paiement

--- src/components/dashboard/paiements/index.jsx
+++ src/components/dashboard/paiements/index.jsx
@@ -2,6 +2,11 @@ export default function Payments(){
     return(
         <>
             <h2>Paiements</h2>
+            <section className="payment-container">
+                <div className="head">
+                    <p>Veulliez vérifier la formation auquel vous allez payer le droit 💳</p>
+                </div>
+            </section>
         </>
     )
 }
