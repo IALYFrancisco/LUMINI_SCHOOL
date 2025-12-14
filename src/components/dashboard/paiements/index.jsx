2272179e87dc1840426717949112b4ce7415dfd6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-14 10:50:51 +0300

    développment encore de la page de paiement de droit

--- src/components/dashboard/paiements/index.jsx
+++ src/components/dashboard/paiements/index.jsx
@@ -1,10 +1,12 @@
+import '../../../../public/styles/dashboard/payment.css'
+
 export default function Payments(){
     return(
         <>
             <h2>Paiements</h2>
             <section className="payment-container">
                 <div className="head">
-                    <p>Veulliez vérifier la formation auquel vous allez payer le droit 💳</p>
+                    <p>Veulliez vérifier la formation auquel vous allez payer le droit 💳.</p>
                 </div>
             </section>
         </>
