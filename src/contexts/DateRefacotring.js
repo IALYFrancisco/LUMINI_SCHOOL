64540cb13076040c4c251811c71183a4f7530e1f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-15 09:11:15 +0300

    méthode réfacteur de date définie

--- src/contexts/DateRefacotring.js
+++ src/contexts/DateRefacotring.js
@@ -1,3 +1,7 @@
-export defaut function DateRefactoring(_date){
-    
+export default function DateRefactoring(_date){
+    const date  = new Date(_date)
+    const pad = (n)=>n.toString().padStart(2, "0")
+    return(
+        date.getFullYear()+"-"+pad(date.getMonth()+1)+"-"+pad(date.getDate())+"T"+pad(date.getHours())+":"+pad(date.getMinutes())
+    )  
 }
