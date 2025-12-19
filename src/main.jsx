c3e06db8d9a0aea07c7af2b65f2187b5d0f0525a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-19 16:56:50 +0300

    configuration de message toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <ToastContainer position="top-right" autoClose={3000}/>
     </AuthProvider>
   </StrictMode>,
 )
