3103bcebc4bb94953e7efad01cc1d58dc7dbcac9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-21 17:24:22 +0300

    suppression des attributs name des champs de saisie

--- src/views/registrations.jsx
+++ src/views/registrations.jsx
@@ -80,30 +80,30 @@ export default function Registrations(){
                                         <legend><h3>A propos de la formation</h3></legend>
                                         <div className="element">
                                             <label htmlFor="">Titre de la formation :</label>
-                                            <input type="text" name="" id="" value={f.title} readOnly />
+                                            <input type="text" id="" value={f.title} readOnly />
                                         </div>
                                         <div className="element">
                                             <label htmlFor="">Les prérequis de la formation :</label>
-                                            <input type="text" name="" id="" value={f.prerequisites} readOnly />
+                                            <input type="text" id="" value={f.prerequisites} readOnly />
                                         </div>
                                         <div className="element">
                                             <label htmlFor="">Déscription de la formation :</label>
-                                            <textarea name="" id="" value={f.description} readOnly ></textarea>
+                                            <textarea id="" value={f.description} readOnly ></textarea>
                                         </div>
                                     </fieldset>
                                     <fieldset>
                                         <legend><h3>Vos informations personnelles</h3></legend>
                                         <div className="element">
                                             <label htmlFor="">Votre nom :</label>
-                                            <input type="text" name="" id="" value={user.name} disabled readOnly />
+                                            <input type="text" id="" value={user.name} disabled readOnly />
                                         </div>
                                         <div className="element">
                                             <label htmlFor="">Votre email :</label>
-                                            <input type="email" name="" id="" value={user.email} disabled readOnly />
+                                            <input type="email" id="" value={user.email} disabled readOnly />
                                         </div>
                                         <div className="element">
                                             <label htmlFor="">Votre numéro téléphone <span className="colored">*</span> :</label>
-                                            <input type="tel" name="" id="" placeholder="ex: 030 00 000 00" { ...register('phoneNumber', { required:true, value: user.phoneNumber ? user.phoneNumber : "" }) } required />
+                                            <input type="tel" id="" placeholder="ex: 030 00 000 00" { ...register('phoneNumber', { required:true, value: user.phoneNumber ? user.phoneNumber : "" }) } required />
                                         </div>
                                     </fieldset>
                                 </div>
