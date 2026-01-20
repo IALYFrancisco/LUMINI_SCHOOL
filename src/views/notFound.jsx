3402e27255180fa2c77e6612751376a2326b1e9b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-20 11:48:57 +0300

    développement de la page 404

--- src/views/notFound.jsx
+++ src/views/notFound.jsx
@@ -1,3 +1,18 @@
+import Nav from "../components/nav"
+import '../../public/styles/notFound.css'
+import { Link } from "react-router-dom"
+
 export function NotFound(){
-    return(<h1>404 not found</h1>)
+    return(<>
+        <Nav/>
+        <section className="not-found-container">
+            <div>
+                <h1>404</h1>
+                <h2>Page introuvable</h2>
+                <Link to="/">
+                    <button>Retour à l'accueil</button>
+                </Link>
+            </div>
+        </section>
+    </>)
 }
