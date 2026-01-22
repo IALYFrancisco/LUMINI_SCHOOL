7957a32c99ffa0a106ce34c230a47fc4fa65b549
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-22 08:18:06 +0300

    configuration du plugin de génération automatique de sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
+import axios from 'axios'
+
+async function GenerateArticleRoutes() {
+  try{
+    let articleRoutes = []
+    let response = await axios.get(`http://localhost:3000/article/get`)
+    response.data.forEach(element => { articleRoutes.push(`/article/${element.slug}`) });
+    return articleRoutes
+  }
+  catch(err){
+    console.log('Error generating article routes for the sitemap file')
+    console.log(err)
+    return
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({ 
+      hostname: "https://luminischool.onrender.com",
+      dynamicRoutes: await GenerateArticleRoutes()
+    }),
+  ],
 })
