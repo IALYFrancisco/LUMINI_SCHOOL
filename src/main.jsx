d0f37d5856e3abd22ab99acc9f3e1402e45ea790
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-23 11:19:55 +0300

    ajout de configuration pour l'utilisation de Helmet

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { Toaster } from 'sonner'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
-      <Toaster position='top-center' richColors/>
+      <HelmetProvider>
+        <App />
+        <Toaster position='top-center' richColors/>
+      </HelmetProvider>
     </AuthProvider>
   </StrictMode>,
 )
