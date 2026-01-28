6ca0df7504715bbfc7ccd7a35ffafdd37c6e53fc
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-28 15:32:31 +0300

    customisation du ImageBlot de Quill

---
+++ src/components/dashboard/articles/CustomImageBlot.js
@@ -0,0 +1,26 @@
+import { Quill } from "react-quill-new";
+
+const BlockEmbed = Quill.import("blots/block/embed")
+
+class ImageBlot extends BlockEmbed {
+    static create(value){
+        const node = super.create()
+        node.setAttribute("src", value.src)
+
+        if(value.alt){
+            node.setAttribute("alt", value.alt)
+        }
+
+        return node
+    }
+
+    static value(node){
+        return {
+            src: node.getAttribute("src"),
+            alt: node.getAttribute("alt")
+        }
+    }
+}
+
+ImageBlot.blotName = "image"
+ImageBlot.tagName = "img"
