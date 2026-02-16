cee0ac12a93b7c5ce29f3a5d9c68aa61743e6ab5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-16 21:28:30 +0300

    ajout des balises meta non dynamiques à la page de lecture d'un article seul

--- src/views/articleView.jsx
+++ src/views/articleView.jsx
@@ -5,15 +5,22 @@ import axios from "axios"
 import Loading from "../components/loading"
 import '../../public/styles/articleView.css'
 import DOMPurify from 'dompurify'
-import { useHead } from "@unhead/react"
+import { useHead, useSeoMeta } from "@unhead/react"
 
 export default function ArticleView(){
 
     useHead({
-        title: 'Installer Windows 11 : configurations requises et bonnes pratiques',
         meta: [
-            { name: 'description', content: 'Découvrez comment installer Windows 11, les configurations requises et les bonnes pratiques pour une installation sécurisée et professionnelle.' }
+            { name: 'description', content: 'Découvrez comment installer Windows 11, les configurations requises et les bonnes pratiques pour une installation sécurisée et professionnelle.' },
+            { name: 'robots', content: 'index, follow' }
         ]
+    })
+
+    useSeoMeta({
+        title: 'Installer Windows 11 : configurations requises et bonnes pratiques',
+        ogType: 'article',
+        ogLocale: 'fr_FR',
+        articleAuthor: 'LUMINI School'
     })
 
     const { slug } = useParams()
