337950d5ea21b3ce0719b97e85b6c64f4e1d1e4a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-16 22:47:42 +0300

    développement encore de la page de modification de SEO de site

--- src/components/dashboard/articles/seo-update.jsx
+++ src/components/dashboard/articles/seo-update.jsx
@@ -1,3 +1,22 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function SEOUpdate(){
-    return <h1>Ceci est une page de mis à jour SEO!</h1>
+    var [article, setArticle] = useState(null)
+    const {id} = useParams()
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/article/get?_id=${id}`, { withCredentials: true })
+            .then((response)=>{
+                setArticle(response.data)
+            }).catch(()=>setArticle(null))
+    },[id])
+    return(
+        article &&
+        <>
+            <div className="update-article-seo">
+                <h3>Modification du SEO de l'article <span>"{article.title}"</span></h3>
+            </div>
+        </>
+    )
 }
