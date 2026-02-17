13cf2b248977069f9ccf2e6bd3bcc90af62acb7d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-17 08:51:36 +0300

    développement de l'interface de mis à jour de SEO

--- src/components/dashboard/articles/seo-update.jsx
+++ src/components/dashboard/articles/seo-update.jsx
@@ -16,6 +16,22 @@ export default function SEOUpdate(){
         <>
             <div className="update-article-seo">
                 <h3>Modification du SEO de l'article <span>"{article.title}"</span></h3>
+                <form>
+                    <fieldset>
+                        <div className="element">
+                            <label htmlFor="page-title">Titre de page [ title, og:title, twitter:title ] :</label>
+                            <input type="text" id="page-title" placeholder="Ajoutez un titre à la page d'article (ce sera également utilisé par og:title et twitter:title)"/>
+                        </div>
+                        <div className="element">
+                            <label htmlFor="page-url">Url de page [ lien canonique, og:url, twitter:url ] :</label>
+                            <input type="url" id="page-url" placeholder="Ajoutez un url canonique à la page d'article (ce sera également utilisé par og:url et twitter:url)"/>
+                        </div>
+                        <div className="element">
+                            <label htmlFor="image">Image de mise en avant [ og:image, twitter:image ] :</label>
+                            <input type="url" id="image" placeholder="Ceci n'est pas modifiable d'ici"/>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
         </>
     )
