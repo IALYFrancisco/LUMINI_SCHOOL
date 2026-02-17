a6220fe5e7bd69b2326949e088d17cc4421018aa
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-17 11:23:13 +0300

    dynamisation de la page de modification de SEO

--- src/components/dashboard/articles/seo-update.jsx
+++ src/components/dashboard/articles/seo-update.jsx
@@ -1,16 +1,34 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { useForm } from "react-hook-form"
 
 export default function SEOUpdate(){
+    
     var [article, setArticle] = useState(null)
+    var [seo, setSeo] = useState(null)
     const {id} = useParams()
+    const { register, handleSubmit, reset } = useForm()
+    
     useEffect(()=>{
         axios.get(`${import.meta.env.VITE_API_BASE_URL}/article/get?_id=${id}`, { withCredentials: true })
             .then((response)=>{
                 setArticle(response.data)
             }).catch(()=>setArticle(null))
     },[id])
+    
+    useEffect(()=>{
+        axios.get(`${import.meta.env.VITE_API_BASE_URL}/seo/get?articleId=${id}`, { withCredentials: true })
+        .then((response)=>{
+            if(response.status === 200){
+                setSeo(response.data)
+            }
+            if(response.status === 209){
+                setSeo(null)
+            }
+        }).catch(()=>setSeo(null))
+    }, [id])
+
     return(
         article &&
         <>
@@ -20,15 +38,15 @@ export default function SEOUpdate(){
                     <fieldset>
                         <div className="element">
                             <label htmlFor="page-title">Titre de page [ title, og:title, twitter:title ] :</label>
-                            <input type="text" id="page-title" placeholder="Ajoutez un titre à la page d'article (ce sera également utilisé par og:title et twitter:title)"/>
+                            <input type="text" id="page-title" placeholder="Ajoutez un titre à la page d'article (ce sera également utilisé par og:title et twitter:title)" { ...register('title') } required/>
                         </div>
                         <div className="element">
                             <label htmlFor="page-url">Url de page [ lien canonique, og:url, twitter:url ] :</label>
-                            <input type="url" id="page-url" placeholder="Ajoutez un url canonique à la page d'article (ce sera également utilisé par og:url et twitter:url)"/>
+                            <input type="url" id="page-url" placeholder="Ajoutez un url canonique à la page d'article (ce sera également utilisé par og:url et twitter:url)" { ...register('canonicUrl') } required/>
                         </div>
                         <div className="element">
                             <label htmlFor="image">Image de mise en avant [ og:image, twitter:image ] :</label>
-                            <input type="url" id="image" placeholder="Ceci n'est pas modifiable d'ici"/>
+                            <input type="url" id="image" placeholder="Ceci n'est pas modifiable d'ici" { ...register('') }/>
                         </div>
                         <div className="element">
                             <button>Soumettre</button>
