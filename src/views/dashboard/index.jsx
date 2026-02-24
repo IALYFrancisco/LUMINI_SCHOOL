cd965fc051cec4a24fea2f662e0517b93892d6a6
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-24 17:44:24 +0300

    ajout de balise meta:robots

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,8 +1,16 @@
 import Sidebar from "../../components/dashboard/sidebar"
 import '../../../public/styles/dashboard/dashboard.css'
 import { Outlet } from "react-router-dom"
+import { useHead } from "@unhead/react"
 
 export default function Dashboard(){
+
+    useHead({
+        meta: [
+            { name: 'robots', content: 'noindex, nofollow' }
+        ]
+    })
+
     return(
         <section id="dashboard-view">
             <Sidebar></Sidebar>
