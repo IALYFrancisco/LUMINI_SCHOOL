6c735d254aa852516bb202801d3a38704b5874a8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-24 16:41:07 +0300

    ajout d'en-tête url

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -5,15 +5,22 @@ import { Footer } from "../components/footer"
 import { Formations } from "../components/formations"
 import { Articles } from "../components/articles"
 import '../../public/styles/home.css'
-import { useSeoMeta } from "@unhead/react"
+import { useHead, useSeoMeta } from "@unhead/react"
 
 function Home(){
+
+    useHead({
+        link: [
+            { rel: 'canonical', href: 'https://luminischool.onrender.com' }
+        ]
+    })
 
     useSeoMeta({
 
         title: 'Accueil | LUMINI School - Plateforme de formation en informatique',
 
         ogTitle: 'Accueil | LUMINI School - Plateforme de formation en informatique',
+        ogUrl: 'https://luminischool.onrender.com',
 
         twitterTitle: 'Accueil | LUMINI School - Plateforme de formation en informatique'
 
