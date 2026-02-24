1b48d305f48ebd12719ea220a16c43bc52baf709
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-24 20:52:39 +0300

    ajout de titre à la page des utilisateurs dans le dashboard

--- src/components/dashboard/users/index.jsx
+++ src/components/dashboard/users/index.jsx
@@ -1,6 +1,12 @@
+import { useHead } from "@unhead/react"
 import { Outlet } from "react-router-dom"
 
 export default function Users(){
+
+    useHead({
+        title: 'Utilisateurs - Dashboard | LUMINI School'
+    })
+
     return(
         <>
             <h2>Utilisateurs</h2>
