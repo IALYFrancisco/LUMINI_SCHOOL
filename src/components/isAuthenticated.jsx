40074c605e66d4f3bec20cf2a0a1e5635b27a58b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-26 06:00:52 +0300

    ajout de toast d'information sur le composant isAuthenticated

--- src/components/isAuthenticated.jsx
+++ src/components/isAuthenticated.jsx
@@ -3,12 +3,16 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import Loading from "./loading";
+import { toast } from "sonner";
 
 export default function IsAuthenticated({children}) {
     const { user, loading } = useAuth()
 
     if(loading) return <Loading/>
-    if(!user) return <Navigate to="/authentication/login" replace/>
+    if(!user){
+        toast.info("Vous devez d'abord vous connecter à votre compte.")
+        return <Navigate to="/authentication/login" replace/>
+    }
 
     return children
 }
